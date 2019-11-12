06260db34e0e3d6dc6797f12bbb45691ecf57881
Daynizyvc <zinyad_leay@hotmail.com>
2019-11-12 14:59:57 -0600

    probando que se puedan ver las imagnes

--- src/views/pages/Contacto.js
+++ src/views/pages/Contacto.js
@@ -5,10 +5,10 @@ let Contacto = {
             <p>
                 Podemos contactar mediante las redes sociales:
             </p>
-            <a href="www.linkedin.com/in/daynizyvc"><img class="img-rs" src="../../../img/linkedin.png"></a>
-            <a href="/"><img class="img-rs" src="../../../img/gmail.png"></a>
-            <a href="/"><img class="img-rs" src="../../../img/github.png"></a>
-            <a href="/"><img class="img-rs" src="../../../img/gorjeo.png"></a>
+            <a href="http://www.linkedin.com/in/daynizyvc"><img class="img-rs" src="../../../img/linkedin.png"></a>
+            <a href="https://twitter.com/dayzzayvc"><img class="img-rs" src="../../../img/gorjeo.png"></a>
+            <a href="https://github.com/daynizyvc"><img class="img-rs" src="../../../img/github.png"></a>
+            <a href="#/error404"><img class="img-rs" src="../../../img/gmail.png"></a>
             </div> 
         `
         return view;
